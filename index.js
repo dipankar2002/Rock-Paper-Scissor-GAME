26cb3c7ef9e34299939305e8606f86bbc9209da2
Dipankar Ganguly <dipankarganguly15@gmail.com>
2024-01-08 23:11:34 +0530

    Updating the JavaScript file

--- index.js
+++ index.js
@@ -11,45 +11,45 @@ function gameLogic() {
   } else {
       if(com == 1 && opt == 2) {
         document.querySelector("h2").innerHTML = "You Won";
-        document.querySelector(".opt2").setAttribute("src","images/stones.png");
-        document.querySelector(".opt1").setAttribute("src","images/papers.png");
+        // document.querySelector(".opt2").setAttribute("src","images/stones.png");
+        // document.querySelector(".opt1").setAttribute("src","images/papers.png");
       } else if(com == 2 && opt == 3) {
         document.querySelector("h2").innerHTML = "You Won";
-        document.querySelector(".opt2").setAttribute("src","images/papers.png");
-        document.querySelector(".opt1").setAttribute("src","images/scissors.png");
+        // document.querySelector(".opt2").setAttribute("src","images/papers.png");
+        // document.querySelector(".opt1").setAttribute("src","images/scissors.png");
       } else if(com == 3 && opt == 1) {
         document.querySelector("h2").innerHTML = "You Won";
-        document.querySelector(".opt2").setAttribute("src","images/scissors.png");
-        document.querySelector(".opt1").setAttribute("src","images/stones.png");
+        // document.querySelector(".opt2").setAttribute("src","images/scissors.png");
+        // document.querySelector(".opt1").setAttribute("src","images/stones.png");
       } else if(com == 2 && opt == 1) {
         document.querySelector("h2").innerHTML = "GohAN Won";
-        document.querySelector(".opt2").setAttribute("src","images/papers.png");
-        document.querySelector(".opt1").setAttribute("src","images/stones.png");
+        // document.querySelector(".opt2").setAttribute("src","images/papers.png");
+        // document.querySelector(".opt1").setAttribute("src","images/stones.png");
       } else if(com == 3 && opt == 2) {
         document.querySelector("h2").innerHTML = "GohAN Won";
-        document.querySelector(".opt2").setAttribute("src","images/scissors.png");
-        document.querySelector(".opt1").setAttribute("src","images/papers.png");
+        // document.querySelector(".opt2").setAttribute("src","images/scissors.png");
+        // document.querySelector(".opt1").setAttribute("src","images/papers.png");
       } else if(com == 1 && opt == 3) {
         document.querySelector("h2").innerHTML = "GohAN Won";
-        document.querySelector(".opt2").setAttribute("src","images/stones.png");
-        document.querySelector(".opt1").setAttribute("src","images/scissors.png");
+        // document.querySelector(".opt2").setAttribute("src","images/stones.png");
+        // document.querySelector(".opt1").setAttribute("src","images/scissors.png");
       }   
   }
 
-  // if(opt == 1) {
-  //   document.querySelector(".opt1").setAttribute("src","images/stones.png");
-  // } else if(opt == 2) {
-  //   document.querySelector(".opt1").setAttribute("src","images/papers.png");
-  // } else if(opt == 3) {
-  //   document.querySelector(".opt1").setAttribute("src","images/scissors.png");
-  // }
+  if(opt == 1) {
+    document.querySelector(".opt1").setAttribute("src","images/stones.png");
+  } else if(opt == 2) {
+    document.querySelector(".opt1").setAttribute("src","images/papers.png");
+  } else if(opt == 3) {
+    document.querySelector(".opt1").setAttribute("src","images/scissors.png");
+  }
 
-  // if(com == 1) {
-  //   document.querySelector(".opt2").setAttribute("src","images/stones.png");
-  // } else if(com == 2) {
-  //   document.querySelector(".opt2").setAttribute("src","images/papers.png");
-  // } else if(com == 3) {
-  //   document.querySelector(".opt2").setAttribute("src","images/scissors.png");
-  // }
+  if(com == 1) {
+    document.querySelector(".opt2").setAttribute("src","images/stones.png");
+  } else if(com == 2) {
+    document.querySelector(".opt2").setAttribute("src","images/papers.png");
+  } else if(com == 3) {
+    document.querySelector(".opt2").setAttribute("src","images/scissors.png");
+  }
 
 }
